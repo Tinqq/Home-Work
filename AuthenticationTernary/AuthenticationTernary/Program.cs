842585daea6d888c26806ba202ff7811bbff95d7
Tinqq <124076113+Tinqq@users.noreply.github.com>
2023-02-07 23:57:14 +0200

    Add files via upload

---
+++ AuthenticationTernary/AuthenticationTernary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AuthenticationTernary
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+ 
+
+                Console.WriteLine("Input login");
+            string username = Console.ReadLine();
+
+            if (username == "USER")
+            {
+                Console.WriteLine("Input Password: ");
+                string password = Console.ReadLine();
+                string hello = (password == "secretPass") ? "Hello User!" : "Password is not correct";
+                Console.WriteLine(hello);
+            }
+
+            if (username == "ADMIN")
+            {
+                Console.WriteLine("Input Password: ");
+                string password = Console.ReadLine();
+                string hello = (password == "secretPass") ? "Hello Administrator!" : "Password is not correct";
+                Console.WriteLine(hello);
+            }
+
+            if (username == "")
+            {
+                Console.WriteLine("Hello Anonimous");
+                
+            }
+
+
+        }
+    }
+}
+
+
+
