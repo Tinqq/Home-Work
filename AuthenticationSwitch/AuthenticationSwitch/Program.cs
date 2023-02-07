55bb69cc6cc8c82c2bd213cbfb62662e36110c39
Tinqq <124076113+Tinqq@users.noreply.github.com>
2023-02-07 23:56:37 +0200

    Add files via upload

---
+++ AuthenticationSwitch/AuthenticationSwitch/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace AuthenticationIfElse
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string username;
+            string password = "secretPass";
+
+            Console.WriteLine("Input login");
+            username = Console.ReadLine();
+
+            switch (username)
+            {
+                case "USER":
+                    Console.WriteLine("Input Password: ");
+                    password = Console.ReadLine();
+                    switch (password )
+                    {
+                        case "secretPass": //ДЛЯ КОРРЕКТНОГО ОТОБРАЖЕНИЯ ПРИВЕТСТВИЯ ПОЛЬЗОВАТЕЛЯ
+                            Console.WriteLine("Hello User");
+                            break;
+                    }
+                    switch (password)
+                    {
+                        case "incorrectPass": //ДЛЯ ОТОБРАЖЕНИЯ УВЕДОМЛЕНИЯ О ВВЕДЕНИИ НЕКОРРЕКТНОГО ПАРОЛЯ
+                            Console.WriteLine("Password is not correct"); ;
+                            break;
+                    }
+                    break;     
+            }
+            switch (username)
+            {
+                case "ADMIN":
+                    Console.WriteLine("Input Password: ");
+                    password = Console.ReadLine();
+
+                    switch (password)
+                    {
+                        case "secretPass": //ДЛЯ КОРРЕКТНОГО ОТОБРАЖЕНИЯ ПРИВЕТСТВИЯ ПОЛЬЗОВАТЕЛЯ
+                            Console.WriteLine("Hello Administrator ");
+                            break;
+                    }
+                    switch (password)
+                    {
+                        case  "incorrectPass": //ДЛЯ ОТОБРАЖЕНИЯ УВЕДОМЛЕНИЯ О ВВЕДЕНИИ НЕКОРРЕКТНОГО ПАРОЛЯ
+                            Console.WriteLine("Password is not correct"); ;
+                            break;
+                    }
+                    break;
+                   
+                   
+            }
+            switch (username)
+            {
+                case "": //ДЛЯ ОТОБРАЖЕНИЯ ПРИВЕТСТВИЯ ПОЛЬЗОВАТЕЛЯ СО ЗНАЧЕНИЕМ НЕИЗВЕСТЕН
+                    Console.WriteLine("Hello Anonimous");
+                    break;
+            }
+
+
+        }
+    }
+}
