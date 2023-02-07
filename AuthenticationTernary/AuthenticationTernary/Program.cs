5c1438e8a1218e0e2d9a0c8b8ee84b2c319cfef4
Tinqq <124076113+Tinqq@users.noreply.github.com>
2023-02-08 00:54:59 +0200

    Update Program.cs

--- AuthenticationTernary/AuthenticationTernary/Program.cs
+++ AuthenticationTernary/AuthenticationTernary/Program.cs
@@ -28,13 +28,28 @@ namespace AuthenticationTernary
                 string hello = (password == "secretPass") ? "Hello Administrator!" : "Password is not correct"; //ДЛЯ ПОЛУЧЕНИЯ СООБЩЕНИЯ О НЕПРАВИЛЬНОМ ВВОДЕ ПАРОЛЯ МОЖНО ВВЕСТИ ЛЮБУЮ СТРОКУ\СИМВОЛ
                 Console.WriteLine(hello);
             }
+            
+            
+            string world = "Hello Anonimous";
+            switch (username)
+            {
+                
+                case "":
+                    Console.WriteLine(world);
+                    break;
+            }
 
-            if (username == "")
+            /*if (username == "")
             {
                 string world = "Hello Anonimous";
                 Console.WriteLine(world);
 
-            }
+            }*/
+            
+            
+            /*string hello2 = (username == "") ? "Hello Anonimous" : ""; - Хотел сделать так, но там постоянно выводит при корректных\некорректных данных в других случаях пустую строку,
+            а это не красиво и неправильно. ПО этому создал переменную и при условии вывел её в консоль через иф\свитч
+            Console.WriteLine(hello2);*/
 
 
         }
