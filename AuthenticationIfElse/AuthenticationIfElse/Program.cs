e572e0dc65f630dbbdcfcabab312d7ae6685944c
Tinqq <124076113+Tinqq@users.noreply.github.com>
2023-02-07 23:55:45 +0200

    Add files via upload

---
+++ AuthenticationIfElse/AuthenticationIfElse/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace AuthenticationIfElse
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string username;
+            string password;
+            
+            
+            
+
+            Console.WriteLine("Input login");
+            username = Console.ReadLine();
+
+           
+
+            if (username == "USER")
+            {
+                Console.WriteLine("Input Password: ");
+                password = Console.ReadLine();
+
+                if(password != "secretPass") //ДЛЯ ОТОБРАЖЕНИЯ УВЕДОМЛЕНИЯ О ВВЕДЕНИИ НЕКОРРЕКТНОГО ПАРОЛЯ
+                {
+                    Console.WriteLine("Password is not correct");
+                }
+
+                else if (password == "secretPass") //ДЛЯ КОРРЕКТНОГО ОТОБРАЖЕНИЯ ПРИВЕТСТВИЯ ПОЛЬЗОВАТЕЛЯ
+                {
+                    Console.WriteLine("Hello User!");
+                }
+
+            }
+            else if (username == "ADMIN")
+            {
+                Console.WriteLine("Input Password: ");
+                password = Console.ReadLine();
+
+                if (password != "secretPass")  //ДЛЯ ОТОБРАЖЕНИЯ УВЕДОМЛЕНИЯ О ВВЕДЕНИИ НЕКОРРЕКТНОГО ПАРОЛЯ
+                {
+                    Console.WriteLine("Password is not correct");
+                }
+
+                 if (password == "secretPass") //ДЛЯ КОРРЕКТНОГО ОТОБРАЖЕНИЯ ПРИВЕТСТВИЯ ПОЛЬЗОВАТЕЛЯ
+                {
+                    Console.WriteLine("Hello Administrator");
+                }
+            }
+            else if (username == "") //ДЛЯ ОТОБРАЖЕНИЯ ПРИВЕТСТВИЯ ПОЛЬЗОВАТЕЛЯ СО ЗНАЧЕНИЕМ НЕИЗВЕСТЕН
+            {
+                Console.WriteLine("Hello Anonimous");
+            }
+
+           
+        }
+    }
+}
