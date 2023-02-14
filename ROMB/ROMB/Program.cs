f6976044199b708218b0bc80b84dce399626286b
Tinqq <124076113+Tinqq@users.noreply.github.com>
2023-02-14 21:25:09 +0200

    Add files via upload

---
+++ ROMB/ROMB/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+
+namespace ROMB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void PrintDiamond(int n = 7)
+            {
+                // Upper half of the diamond
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 0; k <= i; k++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                }
+                // Lower half of the diamond
+                for (int i = n - 2; i >= 0; i--)
+                {
+                    for (int j = 0; j < n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 0; k <= i; k++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            PrintDiamond();
+        }
+    }
+}
